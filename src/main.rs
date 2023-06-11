6d45d3a6957d4b4eb05aa49bf7b094432be131d4
Christopher Trimboli <christopher.trimboli@protonmail.com>
2023-06-11 11:14:03 -0600

    add WS basics, fix cors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+use actix::{Actor, StreamHandler};
 use actix_cors::Cors;
-use actix_web::{get, http::header, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    get, http::header, middleware::Logger, web, App, Error, HttpRequest, HttpResponse, HttpServer,
+    Responder,
+};
+use actix_web_actors::ws;
+
+/// Define HTTP actor
+struct WebSocket;
+
+impl Actor for WebSocket {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocket {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        println!("{:?}", msg);
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+async fn websocket_route(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(WebSocket {}, &req, stream);
+    println!("{:?}", resp);
+    resp
+}
 
 #[get("/hello")]
 async fn hello() -> impl Responder {
@@ -10,10 +41,7 @@ async fn hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         let cors: Cors = Cors::default()
-            .allowed_origin("http://127.0.0.1:8080")
-            .allowed_origin_fn(|origin, _req_head| {
-                origin.as_bytes().starts_with(b"http://localhost")
-            })
+            .allowed_origin("http://localhost:3000")
             .allowed_methods(vec!["GET", "POST"])
             .allowed_headers(&[header::AUTHORIZATION, header::ACCEPT])
             .allowed_header(header::CONTENT_TYPE)
@@ -21,7 +49,11 @@ async fn main() -> std::io::Result<()> {
             .block_on_origin_mismatch(false)
             .max_age(3600);
 
-        App::new().wrap(cors).service(hello)
+        App::new()
+            .wrap(cors)
+            .route("/ws", web::get().to(websocket_route))
+            .service(hello)
+            .wrap(Logger::default())
     })
     .bind(("127.0.0.1", 8080))?
     .run()
